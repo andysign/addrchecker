7381fcdfda94ac73cf8a333a85192b6bc700fb35
andysign <andysignerdesigner@gmail.com>
2025-05-07 20:04:54 +0100

    Cleanup

--- src/fetch-balance-erc721s.ts
+++ src/fetch-balance-erc721s.ts
@@ -1,15 +1,6 @@
 import axios from 'axios'; // FromPostmanSnippet
 
-// import { BN } from "bn.js";
-
-// const qs = require('qs');
-
-// let data = JSON.stringify({ 'owner': '0x4c2F150Fc90fed3d8281114c2349f1906cdE5346' });
-
 export async function fetchBalanceERC721s(address: string, KEY: string) {
-
-const axios = require('axios');
-
   let data = `owner=${address}`;
   let url =
     `https://eth-mainnet.g.alchemy.com/nft/v3/${KEY}/getNFTsForOwner?owner=${address}`
@@ -32,27 +23,3 @@ const axios = require('axios');
     throw error;
   }
 }
-
-// makeRequest();
-
-// export async function fetchBalanceERC721s(address: string, KEY: string) {
-//   const url = `https://api.chainbase.online/v1/account/nfts` +
-//     `?limit=3&page=1&chain_id=1&address=${address}`;
-//   let config = {
-//     method: 'get',
-//     maxBodyLength: Infinity,
-//     url: url,
-//     headers: {
-//       'x-api-key': KEY
-//     },
-//     timeout: 1900
-//   };
-//   try {
-//     const response = await axios.request(config);
-//     const formattedNumNfts = response.data.count;
-//     return `${formattedNumNfts}NFTs`;
-//   } catch (error) {
-//     console.error("ErrFetchBalErc721s" + error);
-//     throw error;
-//   }
-// }
