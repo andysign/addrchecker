94c9ab41879e740737a8163323b114e2dfdfa18a
andysign <andysignerdesigner@gmail.com>
2025-05-07 19:20:20 +0100

    Added dot env and a few default constants like ADDR def, out path def and config  def and described the erg

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,16 @@
 import figlet from "figlet";
 import chalk from "chalk";
 import { Command } from "commander";
+import { default as env } from "dotenv";
+import * as fs from "fs";
 
 const program = new Command();
+
+const ADDRESS_DEFAULT = `0x${"0".repeat(40)}`;
+const OUT_DEFAULT = "./out.csv";
+const CONFIG_DEFAULT = "./.env";
+
+let address = "", output = "", config = "";
 
 program
   .version("1.0.0", "-v, --version", "output the current version")
@@ -12,10 +20,14 @@ program
     figlet.textSync("AddrChecker") + "\n" +
     "Checks the metadata of an 0x address and saves the data in a CSV"
   )
+  .argument('<address>', 'Specify the required 42-length address, e.g: ' + ADDRESS_DEFAULT)
   .parse(process.argv);
 
 const options = program.opts();
 
-if (!process.argv.slice(2).length) {
-  program.outputHelp();
+const args = program.args;
+address = args[0];
+if (address && address.length !== 42) {
+  console.error(chalk.red("Error: Address must be exactly 42 characters long."));
+  process.exit(1);
 }
